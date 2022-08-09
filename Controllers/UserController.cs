01040b7808d0e89062e10040277e5e1dc7bff94d
felegy <felegy@hvg.hu>
2022-08-09 11:37:27 +0200

    + Cloudflare Auth

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -15,32 +15,13 @@ namespace JwtApp.Controllers
     public class UserController : ControllerBase
     {
 
-        [HttpGet("Admins")]
-        [Authorize(Roles = "Administrator")]
-        public IActionResult AdminsEndpoint()
-        {
-            var currentUser = GetCurrentUser();
-
-            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
-        }
-
-
-        [HttpGet("Sellers")]
-        [Authorize(Roles = "Seller")]
-        public IActionResult SellersEndpoint()
-        {
-            var currentUser = GetCurrentUser();
-
-            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
-        }
-
-        [HttpGet("AdminsAndSellers")]
-        [Authorize(Roles = "Administrator,Seller")]
+        [HttpGet("Authorized")]
+        [Authorize]
         public IActionResult AdminsAndSellersEndpoint()
         {
             var currentUser = GetCurrentUser();
 
-            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+            return Ok($"Hi {currentUser.EmailAddress}!");
         }
 
         [HttpGet("Public")]
@@ -59,11 +40,7 @@ namespace JwtApp.Controllers
 
                 return new UserModel
                 {
-                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
-                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
-                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
-                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
-                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value
                 };
             }
             return null;
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,17 +29,39 @@ namespace JwtApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            System.Net.Cache.RequestCacheLevel cacheLevel = System.Net.Cache.RequestCacheLevel.Default;
+            Enum.TryParse<System.Net.Cache.RequestCacheLevel>(Configuration["Jwt:JwkCacheLevel"], out cacheLevel);
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
+                        IssuerSigningKeyResolver =
+                        (s, securityToken, identifier, parameters) =>
+                        {
+                            // Get JsonWebKeySet from Issuer https://www.rfc-editor.org/rfc/rfc7517
+                            // https://developers.cloudflare.com/cloudflare-one/identity/users/access-jwt/validating-json/
+                            using (var webClient = new WebClient())
+                            {
+                                webClient.CachePolicy = new System.Net.Cache.RequestCachePolicy(cacheLevel);
+                                webClient.BaseAddress = parameters.ValidIssuer;
+                                return JsonWebKeySet.Create(webClient.DownloadString(Configuration["Jwt:JwkEndpoint"])).Keys;
+                            }
+                        },
+                        ValidateIssuer = true,
                         ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidateAudience = true,
                         ValidAudience = Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                        ValidateLifetime = true
+                    };
+                    options.Events = new JwtBearerEvents()
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            context.Token = context.HttpContext.Request.Headers[Configuration["Jwt:Header"]];
+                            return Task.CompletedTask;
+                        }
                     };
                 });
             services.AddMvc();
@@ -58,7 +83,6 @@ namespace JwtApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
